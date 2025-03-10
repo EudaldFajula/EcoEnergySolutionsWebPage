3c8542d841da15ecf056264ed8d123fddd5351ac
Eudald Fajula <eudald.fajula.7e6@itb.cat>
2025-03-10 19:20:10 +0100

    Crear classes per a simulacio. Closes #2

---
+++ EcoEnergySolutionsWebPage/Classes/Simulation/SistemaEnergia.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+
+
+namespace EcoEnergyWeb
+{
+	public enum TipusEnergiaEnum
+	{
+		Solar,
+		Hidroelectrica,
+		Eolica
+	}
+
+	public class SistemaEnergia
+	{
+		public System.DateTime Data { get; set; }
+
+		[EnumDataType(typeof(TipusEnergiaEnum))]
+		public TipusEnergiaEnum TipusEnergia { get; set; }
+		public int Rati { get; set; }
+		public double Cost { get; set; }
+		public double Preu { get; set; }
+	}
+}
---
+++ EcoEnergySolutionsWebPage/Classes/Simulation/SistemaEolic.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+
+namespace EcoEnergyWeb
+{
+    public class SistemaEolic : SistemaEnergia
+    {
+        public double VelocitatVent { get; set; }
+        public SistemaEolic(double velocitatVent, double rati, double cost, double preu)
+        {
+            VelocitatVent = velocitatVent;
+            Cost = cost;
+            Preu = preu;
+            Data = DateTime.Now;
+            TipusEnergia = TipusEnergiaEnum.Eolica;
+        }
+        public SistemaEolic() 
+        {
+			Data = DateTime.Now;
+			TipusEnergia = TipusEnergiaEnum.Eolica;
+		}
+    }
+}
